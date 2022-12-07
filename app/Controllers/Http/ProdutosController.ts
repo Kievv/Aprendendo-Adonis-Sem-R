c842dac409b93914ac6b2d6405c5061f8336b299
Caio Silva 2024290004 <caioc.silva96@gmail.com>
2022-12-06 21:54:17 -0300

    Adicionando CRUD básico

--- app/Controllers/Http/ProdutosController.ts
+++ app/Controllers/Http/ProdutosController.ts
@@ -1,8 +1,27 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ProdutosController {
-  public async index() {}
-  public async novo() {}
-  public async editar() {}
-  public async remover() {}
+  static produtos = [
+    {
+      nome: "Pão",
+      preco: 1.0,
+    },
+  ];
+
+  public async index({ params }) {
+    if (params.id) {
+      return ProdutosController.produtos[params.id];
+    }
+    return ProdutosController.produtos;
+  }
+  public async novo({ request }) {
+    ProdutosController.produtos.push(request.body());
+  }
+  public async editar({ request }) {
+    const { id } = request.params();
+    ProdutosController.produtos[id] = request.body();
+  }
+  public async remover({ params }) {
+    ProdutosController.produtos.splice(params.id, 1);
+  }
 }
