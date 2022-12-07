e209763a6cf76884d06848091cbac61b56d2dda2
Caio Silva 2024290004 <caioc.silva96@gmail.com>
2022-12-06 21:19:05 -0300

    Aprendendo local de rotas e controllers na estrutura de pastas do Adonis

---
+++ start/routes.ts
@@ -0,0 +1,35 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from "@ioc:Adonis/Core/Route";
+
+Route.get("/", async () => {
+  return { hello: "world" };
+});
+
+// Rotas vão aqui e usando o comando no terminal node ace list:routes conseguimos acessar a estrutura de rotas da API
+
+Route.group(() => {
+  Route.get("/produtos", "Controller.method");
+  Route.get("/produtos/{id}", "Controller.method");
+  Route.post("/produtos", "Controller.method");
+  Route.put("/produtos/{id}", "Controller.method");
+  Route.delete("/produtos/{id}", "Controller.method");
+});
